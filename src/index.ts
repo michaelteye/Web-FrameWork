6668e8f2a60befae0c6774a6532cc6af3e540a2c
michaelteye <michaelteyegyemah@gmail.com>
2023-07-28 07:25:40 +0000

    adding all

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import { User } from "./models/User";
+// import { User } from "./models/User";
 
-const user = new User({ name:'makes', age:30})
+// const user = new User({ name:'makes', age:30})
 
-user.on('change', ()=>{
-    console.log('Change 1')
-})
-user.on('change',()=>{
-    console.log('Change 2')
-})
-user.on('tehrue',()=>{
-    console.log('Change 3')
-})
+// user.on('change', ()=>{
+//     console.log('Change 1')
+// })
+// user.on('change',()=>{
+//     console.log('Change 2')
+// })
+// user.on('tehrue',()=>{
+//     console.log('Change 3')
+// })
 
-user.trigger('change')
+// user.trigger('change')
+
+import axios from 'axios'
+axios.post('http://localhost:3000/users',{
+    name:'Michael',
+    age:20
+})
